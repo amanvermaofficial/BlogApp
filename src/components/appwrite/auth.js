6580fe52a6d82dcc71b8d673b2835c7e340bdfa9
Aman Verma <amn.lohaniwal@gmail.com>
2024-12-22 20:52:59 +0530

    (setup):setup auth and storage with appwrite and setup redux store

---
+++ src/components/appwrite/auth.js
@@ -0,0 +1,65 @@
+import conf from '../conf/conf'
+import { Client, Account, ID } from "appwrite"
+
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client 
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteUrl)
+        
+        this.account=new Account(this.client)
+
+    }
+
+    async  createAccount({email,password,name}){
+      try{
+       const userAccount = this.account.create(ID.unique(),email,password,name)
+       if(userAccount){
+        return this.login({email,password})
+       }
+       else{
+        return userAccount
+       }
+      }
+      catch(err){
+        throw err;
+      }
+    }
+ 
+    async login({email,password}){
+        try{
+            return await this.account.createEmailPasswordSession(email,password);
+        }
+        catch(err){
+            throw err
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch(err){
+             throw err
+        }
+        return null;
+    }
+
+    async logout(){
+        try{
+            await this.account.deleteSessions();
+            
+        }
+        catch(err){
+            console.log("Appwrite service::logout::error",err ); 
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default authService;
---
+++ src/components/appwrite/config.js
@@ -0,0 +1,136 @@
+import conf from '../conf/conf.js'
+import { Client,ID,Databases,Storage,Query } from 'appwrite'
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appwriteProjectId);
+            this.databases=new Databases(this.client)
+            this.bucket=new Storage(this.client)
+    }
+
+    async createPost({title,slug,content,fearuredImage,status,userId}){
+        try{
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    fearuredImage,
+                    status,
+                    userId
+                }
+            )
+        }
+        catch(err){
+            console.log(err);     
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try{
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true
+        }
+        catch(err){
+            console.log(err);
+            return false
+        }
+    }
+
+    async getPost(slug){
+        try{
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        }
+        catch(err){
+            console.log("Err in getPost:",err);
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try{
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        }
+        catch(err){
+            console.log("appwrite service :: getPosts :: error",err);
+            return false
+        }
+    }
+
+    //file uploads
+
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        }catch (error) {
+        console.log("appwrite service :: uploadFile :: error",error);
+        return false
+      }
+
+    }
+
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+              )
+              return true
+        }
+        catch(error){
+            console.log("appwrite service :: deleteFile :: error",error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+          conf.appwriteBucketId,
+          fileId,
+        )
+    }
+}
+
+const service = new Service();
+export default service 
